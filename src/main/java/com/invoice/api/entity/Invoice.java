59cf89fb0a56369f152c1e6607c76d85244d09d8
Jesús Ivan Saavedra Martinez <isaavedra@grupopm.mx>
2025-11-02 21:09:38 -0600

    first commit

---
+++ src/main/java/com/invoice/api/entity/Invoice.java
@@ -0,0 +1,107 @@
+package com.invoice.api.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "invoice")
+public class Invoice {
+	
+	@Id
+	private String invoice_id;
+
+	private Integer user_id;
+	
+	private String created_at;
+
+	private Double subtotal;
+
+	private Double taxes;
+
+	private Double total;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "invoice_id", referencedColumnName = "invoice_id")
+	private List<InvoiceItem> items;
+	
+	public Invoice() {
+		
+	}
+
+	public Invoice(String invoice_id, Integer user_id, String created_at, Double subtotal, Double taxes, Double total,
+			List<InvoiceItem> items) {
+		super();
+		this.invoice_id = invoice_id;
+		this.user_id = user_id;
+		this.created_at = created_at;
+		this.subtotal = subtotal;
+		this.taxes = taxes;
+		this.total = total;
+		this.items = items;
+	}
+
+	public String getInvoice_id() {
+		return invoice_id;
+	}
+
+	public void setInvoice_id(String invoice_id) {
+		this.invoice_id = invoice_id;
+	}
+
+	public Integer getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+
+	public Double getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(Double subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public Double getTaxes() {
+		return taxes;
+	}
+
+	public void setTaxes(Double taxes) {
+		this.taxes = taxes;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+	public List<InvoiceItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<InvoiceItem> items) {
+		this.items = items;
+	}
+
+	
+}
---
+++ src/main/java/com/invoice/api/service/SvcInvoiceImp.java
@@ -0,0 +1,75 @@
+package com.invoice.api.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import com.invoice.api.dto.ApiResponse;
+import com.invoice.api.dto.DtoInvoiceList;
+import com.invoice.api.entity.Invoice;
+import com.invoice.api.repository.RepoInvoice;
+import com.invoice.commons.mapper.MapperInvoice;
+import com.invoice.commons.util.JwtDecoder;
+import com.invoice.exception.ApiException;
+import com.invoice.exception.DBAccessException;
+
+@Service
+public class SvcInvoiceImp implements SvcInvoice {
+	
+	@Autowired
+    private RepoInvoice repo;
+	
+	@Autowired
+	private JwtDecoder jwtDecoder;
+	
+	@Autowired
+	MapperInvoice mapper;
+
+	@Override
+	public List<DtoInvoiceList> findAll() {
+		try {
+			if(jwtDecoder.isAdmin()) {
+				return mapper.toDtoList(repo.findAll());
+			}else {
+				Integer user_id = jwtDecoder.getUserId();
+				return mapper.toDtoList(repo.findAllByUserId(user_id));
+			}
+		}catch (DataAccessException e) {
+	        throw new DBAccessException();
+	    }
+	}
+
+	@Override
+	public Invoice findById(Integer id) {
+		try {
+			Invoice invoice = repo.findById(id).get();
+			if(!jwtDecoder.isAdmin()) {
+				Integer user_id = jwtDecoder.getUserId();
+				if(invoice.getUser_id() != user_id) {
+					throw new ApiException(HttpStatus.FORBIDDEN, "El token no es válido para consultar esta factura");
+				}
+			}
+			return invoice;
+		}catch (DataAccessException e) {
+	        throw new DBAccessException();
+	    }catch (NoSuchElementException e) {
+			throw new ApiException(HttpStatus.NOT_FOUND, "El id de la factura no existe");
+	    }
+	}
+
+	@Override
+	public ApiResponse create() {
+		try {
+			/*
+			 * Proyecto: Requerimiento 2 - Finalizar compra
+			 */
+			return new ApiResponse("La factura ha sido registrada");
+		}catch (DataAccessException e) {
+	        throw new DBAccessException();
+	    }
+	}
+}
